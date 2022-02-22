55a7fab810ee88f928831fd8a69a4e9aaa7c153b
Norberto Contreras <88049567+Nordevelop@users.noreply.github.com>
2022-02-21 23:23:53 -0300

    Update selectionsort.java

--- selectionsort.java
+++ selectionsort.java
@@ -8,34 +8,38 @@ public class Main{
     /* Algoritmo de Ordenamiento por metodo Seleccion
     *  Author Nordevelop
     */
-    int cont= 15;
+    int cont=15; 
     long aux;
-    int a[]={15,10,3,12,7,13,8,9,11,2,5,14,1,4,6};
-    int entrada, salida, minimo, temp;
-    System.out.println("Array Desordenado");
-       for(int i=0;i<cont;i++){
-         System.out.print(a[i]);
-				 System.out.print(" ");
-       }//end For
-    for(salida=0;salida < cont;salida++){
-       if (a[entrada]< a[minimo])
-          minimo =entrada;
-       for(entrada=salida+1;entrada <cont; entrada++){
-           if(a[entrada] < a[minimo] )   
-				   	 minimo = entrada;
-       }//end for entrada
-      //intercambio
+    int a[]= {15,10,3,12,7,13,8,9,11,2,5,14,1,4,6}; //generacion del Array
+    int uno = 0;   
+    int dos = 0;
+    int temp;
+    int entrada, salida, minimo;	  
+    System.out.println("Array Desordenado ");
+    for(int i=0;i< cont;i++) {
+	System.out.print(a[i]);
+	System.out.print(" ");
+    } //end for
+    for(salida=0; salida<cont-1; salida++){ 
+        minimo = salida; // minimo
+        for(entrada=salida+1; entrada<cont; entrada++) { 
+	    if(a[entrada] < a[minimo] )   // if minimo es mayor,
+                 minimo = entrada;
+	}
+      /* Intercambiar el elemento con valor mínimo --> 
+      * por la primera posición del array que todavia no está ordenado. 
+      */
        temp = a[minimo]; 
-		   a[minimo] = a[salida]; 
-		   a[salida] = temp;
-    }//end for (salida)
-    System.out.println(" ");
-		System.out.println("Array Ordenado Con el metodo de Seleccion ");
-		for (int j=0;j<cont;j++) {
-			System.out.print(a[j] +"");
-			System.out.print(" ");
-    }
-     
-  
+       a[minimo] = a[salida]; 
+       a[salida] = temp; 		 
+       } // fin for
+       System.out.println(" ");
+       System.out.println("Array Ordenado Con el metodo de Selección ");
+       for (int j=0;j<cont;j++) {
+            System.out.print(a[j] +"");
+            System.out.print(" ");
+       }
+
   }
-}
+	
+}	
